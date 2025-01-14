04435ff1fa41650650872fc0799e8b27cda81863
Ahmed bdiwy <ahmedbedawi22@gmail.com>
2025-01-14 22:47:40 +0200

    update in side bar

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -9,7 +9,7 @@
   </div>
   <hr class="horizontal dark mt-0">
   <div class="collapse navbar-collapse  w-auto" id="sidenav-collapse-main">
-    <ul class="navbar-nav">
+    <ul class="navbar-nav custom-scrollbar" style="position: fixed; overflow-y: auto; height: calc(100vh - 120px);">
       <li class="nav-item">
         <a class="nav-link {{ (Request::is('dashboard') ? 'active' : '') }}" href="{{ url('dashboard') }}">
           <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
