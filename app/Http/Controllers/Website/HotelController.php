99f111446134a1c5b4fc8098327d88d40fc0f257
Ahmed bdiwy <ahmedbedawi22@gmail.com>
2025-03-21 16:11:57 +0200

    refactor: reorganize authentication and dashboard controllers, remove obsolete files, and introduce new website controllers

--- app/Http/Controllers/Website/HotelController.php
+++ app/Http/Controllers/Website/HotelController.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Website;
+use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
--- app/Http/Controllers/Website/StaticController.php
+++ app/Http/Controllers/Website/StaticController.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Website;
+use App\Http\Controllers\Controller;
 
 class StaticController extends Controller
 {
